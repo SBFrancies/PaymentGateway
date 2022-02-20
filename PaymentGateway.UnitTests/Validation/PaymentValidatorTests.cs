bdbe8d13b514e4f071e7e552db958052cc6ea57a
Simon Francies <simon_francies@hotmail.com>
2022-02-20 11:31:53 +0000

    Try moving dockerfiles

---
+++ PaymentGateway.UnitTests/Validation/PaymentValidatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using PaymentGateway.Api.Validation;
+
+namespace PaymentGateway.UnitTests.Validation
+{
+    public class PaymentValidatorTests
+    {
+        private readonly Mock<ISystemClock> _mockClock = new Mock<ISystemClock>();
+
+
+        private PaymentValidator GetSystemUnderTest()
+        {
+            return new PaymentValidator(_mockClock.Object);
+        }
+    }
+}
