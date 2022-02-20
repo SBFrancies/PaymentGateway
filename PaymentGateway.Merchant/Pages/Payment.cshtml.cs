22da0876960d2a8b2776cb67dc5485e2055766d5
Simon Francies <simon_francies@hotmail.com>
2022-02-20 17:45:56 +0000

    Make pages

--- PaymentGateway.Merchant/Pages/Payment.cshtml.cs
+++ PaymentGateway.Merchant/Pages/Payment.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PaymentGateway.Merchant.Interface;
+using PaymentGateway.Merchant.Models.ApiModels;
+using System;
+using System.Threading.Tasks;
 
 namespace PaymentGateway.Merchant.Pages
 {
     public class PaymentModel : PageModel
     {
-        public void OnGet()
+        private IGatewayApi GatewayApi {get;}
+
+        [BindProperty]
+        public Payment Payment { get; set; }
+
+        public PaymentModel(IGatewayApi gatewayApi)
         {
+            GatewayApi = gatewayApi ?? throw new ArgumentNullException(nameof(gatewayApi));
+        }
+
+        public async Task<IActionResult> OnGetAsync([FromRoute] Guid id)
+        {
+            Payment = await GatewayApi.GetAsync(id);
+
+            if(Payment == null)
+            {
+                return RedirectToPage("ViewPayments");
+            }
+
+            return Page();
         }
     }
 }
--- PaymentGateway.Merchant/Pages/ViewPayments.cshtml.cs
+++ PaymentGateway.Merchant/Pages/ViewPayments.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PaymentGateway.Merchant.Interface;
+using PaymentGateway.Merchant.Models.ApiModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace PaymentGateway.Merchant.Pages
 {
     public class ViewPaymentsModel : PageModel
     {
-        public void OnGet()
+        private IGatewayApi GatewayApi { get; }
+
+        [BindProperty]
+        public IEnumerable<Payment> Payments { get; set; }
+
+        public ViewPaymentsModel(IGatewayApi gatewayApi)
         {
+            GatewayApi = gatewayApi ?? throw new ArgumentNullException(nameof(gatewayApi));
+        }
+
+
+        public async Task<IActionResult> OnGetAsync([FromQuery]string cardNumber, [FromQuery] string reference)
+        {
+            Payments = await GatewayApi.GetAsync(cardNumber, reference);
+
+            return Page();
         }
     }
 }
