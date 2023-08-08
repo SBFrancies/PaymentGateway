2ceb3c26af3eefe0717dc46be733b4de097ca75f
Simon Francies <simon_francies@hotmail.com>
2023-08-08 04:32:49 +0100

    Add rootcontrollers

---
+++ PaymentGateway.BankSimulator/Controllers/RootController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentGateway.BankSimulator.Controllers
+{
+    [Route("/")]
+    [AllowAnonymous]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class RootController : ControllerBase
+    {        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("PaymentGateway Bank Simulator");
+        }
+    }
+}
---
+++ PaymentGateway.Merchant/Controllers/RootController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentGateway.Merchant.Controllers
+{
+    [Route("/")]
+    [AllowAnonymous]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class RootController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("PaymentGateway Merchant");
+        }
+    }
+}
